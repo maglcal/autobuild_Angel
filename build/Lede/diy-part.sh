c77d9dee03dc20df8cb9b4f6ae4f2bf187998287
丶许诺 <rainbow-cui@163.com>
2023-02-12 16:58:14 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -51,7 +51,7 @@ export Cancel_running="0"                    # 取消路由器每天跑分任务
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-https://github.com/281677160/openwrt-package.git package/
+git clone https://github.com/281677160/openwrt-package.git package/Angel/
 
 # 修改插件名字
 sed -i 's/"aMule设置"/"电驴下载"/g' `egrep "aMule设置" -rl ./`
